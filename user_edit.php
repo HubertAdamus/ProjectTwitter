79947a6e5743b2535ff98889118df26115d219d8
Hubert <hubert.adamus@gmail.com>
2015-07-22 01:02:29 +0200

    Comeback to basics after fatal bootstrap after-effects

--- user_edit.php
+++ user_edit.php
@@ -5,20 +5,22 @@ $loggedUser = new User();
 $loggedUser->loadFromDB($conn, $_SESSION["user_id"]);
 
 if($_SERVER["REQUEST_METHOD"] == 'POST') {
-    $loggedUser->saveToDB($conn, $_POST["desc"], $_POST["password"], $_POST["password2"]);
+    $loggedUser->updateUser($conn, $_POST["desc"], $_POST["password"], $_POST["password2"]);
 }
 
 
 
 ?>
-<form method="post" action="#">
-    <label>Description</label><br>
-    <input type="text" name="desc" maxlength="255" value=""/><br>
-    <label>Hasło</label><br>
-    <input type="password" name="password" maxlength="255" value=""><br>
-    <label>Powtórz hasło</label><br>
-    <input type="password" name="password2" maxlength="255"  value=""><br>
-    <button type="submit">Wyślij</button>
+<form method="post" style="padding: 20px">
+    <label>CHANGE DESCRIPTION</label><br>
+    <label></label><br>
+    <input type="text" name="desc" maxlength="255"placeholder="Description"/><br>
+    <label></label><br>
+    <input type="password" name="password" maxlength="255" placeholder="Password"><br>
+    <label></label><br>
+    <input type="password" name="password2" maxlength="255"  placeholder="Password again"><br>
+    <label></label><br>
+    <input type="submit" value="Send"/>
 </form>
 
 
