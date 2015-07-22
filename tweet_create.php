c1be611090cd59f0b5ca7d418cbcbcf918fb18cf
Hubert <hubert.adamus@gmail.com>
2015-07-23 00:44:58 +0200

    Some changes

--- tweet_create.php
+++ tweet_create.php
@@ -1,32 +1,25 @@
 <?php
-header ('Content-type: text/html; charset=utf-8');
-require_once ("src/tweet.php");
-require_once ("conn.php");
-session_start();
 
-if($_SERVER["REQUEST_METHOD"] == 'POST'){
+if($_SERVER["REQUEST_METHOD"] === 'POST'){
     var_dump($_POST);
     echo "<br>";
     $newTweet = new Tweet();
-    $newTweet->create($conn, $_POST["tweet"]);
+    $newTweet->createTweet($conn, $_POST["tweet"]);
 
     if($newTweet->getId() != -1){
         $_SESSION["user_id"]=$newTweet->getId();
-        header ("Location: http://localhost/Exercise/Twitter/");
         die();
     }else{
         echo "Error during creating tweet.<br>";
     }
 }
-
 ?>
 
 
-
-
-<form method="post" action="#" id="usrform">
-    <label>Wprowadź tweeta</label><br>
-    <textarea name="tweet" form="usrform" placeholder="Enter Tweet here"></textarea><br>
-    <input type="submit" value="Wyślij">
+<form method="post" action="#" style="padding: 20px"/>
+    <label></label><br>
+    <textarea name="tweet" form="usrform" placeholder="Enter tweet here"></textarea><br>
+    <label</label><br>
+    <input type="submit" value="Send">
 </form>
 
